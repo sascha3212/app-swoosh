d53976235dcb4161c819a23b4acaedacb37abde1
Sascha Landegge <saschalandegge@live.nl>
2018-01-03 20:25:54 +0100

    finished app

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Sascha Landegge on 03/01/2018.
+//  Copyright © 2018 Codeunicum. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
